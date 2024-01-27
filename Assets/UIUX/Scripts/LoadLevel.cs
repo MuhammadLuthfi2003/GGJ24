b85437fdec0c65c3bd1cf8b21901f530a21d0c09
Adib Nafisudin <47674558+Adib-Nafisudin@users.noreply.github.com>
2024-01-27 14:29:51 +0700

    UI Game

--- Assets/UIUX/Scripts/LoadLevel.cs
+++ Assets/UIUX/Scripts/LoadLevel.cs
@@ -1,18 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class LoadLevel : MonoBehaviour
+public class LevelLoader : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // Function to load the specified level by name
+    public void LoadLevel(string levelToLoad)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene(levelToLoad);
     }
 }
