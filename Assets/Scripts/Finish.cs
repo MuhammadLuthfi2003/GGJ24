054042bd8268e0745f9f34e3b7555a990b3aba13
MuhammadLuthfi2003 <muhammad.luthfi2003@mail.ugm.ac.id>
2024-01-27 19:49:41 +0700

    Implement Asset

---
+++ Assets/Scripts/Finish.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Finish : MonoBehaviour
+{
+    public UnityEngine.Events.UnityEvent onHit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.GetComponent<Ball>() != null)
+        {
+            onHit.Invoke();
+        }
+    }
+}
